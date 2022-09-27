dee849a316174905874e50f131fca778fcb8c8ce
Jose Vitor <j.vitorcalegaro@gmail.com>
2022-09-27 14:27:58 -0300

    Alteração

--- exercicio_02.py
+++
@@ -1,3 +0,0 @@
-nota1 = int(input('insira a primeira nota: '))
-nota2 = int(input('insira a segunda nota: '))
-print('a media eh: ', (nota1 + nota2) / 2)
--- exercicio_03.py
+++
@@ -1,8 +0,0 @@
-def imparPar(v1, v2):
-    v1 = v1 + v2
-    if v1%2 == 0: print('A soma resulta em um numero | PAR |')
-    else: print('A soma resulta em um numero | IMPAR |')
-
-valor1 = int(input('insira o primeiro valor: '))
-valor2 = int(input('insira o segundo valor: '))
-imparPar(valor1, valor2)
